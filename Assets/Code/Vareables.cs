99ab873c329c55c7d2471cc8aba451d0c0f3a2dd
mrGrANTt <sambistyyy0@gmail.com>
2026-01-04 23:40:36 +1000

    Last two commit... idk why

--- Assets/Code/Vareables.cs
+++ Assets/Code/Vareables.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 public static class Vareables
@@ -5,5 +7,43 @@ public static class Vareables
     public static float Sound = 1f;
     public static float Music = 1f;
 
-    public static void init() { }
+    public static string path = Application.persistentDataPath 
+        + "/player.json";
+
+    [Serializable]
+    public class Data
+    {
+        public float Sound;
+        public float Music;
+
+        public Data(float sound, float music)
+        {
+            Sound = sound;
+            Music = music;
+        }
+        public Data() : this(1f, 1f) { }
+    }
+
+    public static void loadData()
+    {
+        Data newData;
+        try {
+            string data = File.ReadAllText(path);
+            newData = JsonUtility.FromJson<Data>(data);
+        } catch (Exception e) {
+            Debug.LogException(e);
+            Debug.Log("Generate default values");
+            newData = new Data();
+        }
+
+        Sound = newData.Sound;
+        Music = newData.Music;
+    }
+
+    public static void saveData()
+    {
+        Data data = new Data(Sound, Music);
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(path, json);
+    }
 }
