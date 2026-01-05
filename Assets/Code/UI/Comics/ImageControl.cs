bf81253c18b7791d8e0a3bfa96d5ad69326423b1
mrGrANTt <sambistyyy0@gmail.com>
2026-01-05 10:01:56 +1000

    Comics menu(demo)

---
+++ Assets/Code/UI/Comics/ImageControl.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ImageControl : MonoBehaviour
+{
+    public Sprite[] textures;
+    public float newPlateTime = 10f;
+    public float fadeTime = 1f;
+
+    private Image image;
+    private float time = -3;
+    private int lastPlate = 0;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Update() {
+        if(image == null) return;
+        time += Time.deltaTime;
+
+        if (time >= newPlateTime-fadeTime) {
+            if (time < newPlateTime) {
+                Color color = new Color(1f, 1f, 1f, (newPlateTime-time)/fadeTime);
+                image.color = color;
+            } else {
+                Color color = new Color(1f, 1f, 1f, 0f);
+                image.color = color;
+                if (lastPlate >= textures.Length) {
+                    SceneManager.LoadScene("Game");
+                    return;
+                }
+                time = 0;
+                image.sprite = textures[lastPlate++];
+            }
+        } else {
+            Color color;
+            if (time <= fadeTime) 
+                color = new Color(1f, 1f, 1f, time/fadeTime);
+            else 
+                color = Color.white;
+            image.color = color;
+        }
+    }
+}
--- Assets/Code/UI/ManeMenu/ButtonControl.cs
+++ Assets/Code/UI/ManeMenu/ButtonControl.cs
@@ -8,8 +8,8 @@ public class ButtonControl : MonoBehaviour
     public GameObject setings, buttons;
     public Text screenText;
 
-    public void Play() { SceneManager.LoadScene("Play"); }
-    public void NewGame() { SceneManager.LoadScene("NewGame"); }
+    public void Play() { SceneManager.LoadScene("Game"); }
+    public void NewGame() { SceneManager.LoadScene("Comics"); }
     public void Setings() {
         setings.SetActive(true);
         buttons.SetActive(false);
